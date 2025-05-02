d3e94d6b73fdad6fc2fe097064d8f0a5addd1d70
rocketbro <asherpope@gmail.com>
2025-05-02 11:00:40 -0400

    update sanity config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'o1brandp',
     dataset: 'private'
   },
+  studioHost: 'sophronstudies',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
